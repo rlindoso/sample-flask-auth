0be8edb8b21f8bc4e1d4ca092d664db79ba9602e
Rafael Lindoso <rafael.lindoso@gmail.com>
2024-12-31 12:40:16 -0300

    Create user model

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models import user
+from database import db
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
-
+db.init_app(app)
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
